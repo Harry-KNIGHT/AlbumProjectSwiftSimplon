3d8409d2123177d4c543570486bb8d1bb046d083
Mickaël MAS <appiwedia@gmail.com>
2021-09-14 11:43:00 +0200

    Initial Commit

---
+++ AlbumProject/AlbumProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumProjectApp.swift
+//  AlbumProject
+//
+//  Created by Mickael Mas on 14/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlbumProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
